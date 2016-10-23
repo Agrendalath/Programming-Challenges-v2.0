834dbf14bdf19b6f513ff3b8fb50b416422f5e53
Agrendalath <Agrendalath@gmail.com>
2016-10-23 15:11:53 +0200

    Cosmetic changes (imports).

--- java/src/agrendalath/rock_paper_scissors/RPSLSTest.java
+++ java/src/agrendalath/rock_paper_scissors/RPSLSTest.java
@@ -2,7 +2,8 @@ package agrendalath.rock_paper_scissors;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class RPSLSTest {
     @Test
